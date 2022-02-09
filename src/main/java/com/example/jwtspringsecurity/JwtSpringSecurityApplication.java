41bd4242ca6808b36bda51ec3d805befb23c284f
Tsonewa <tsonewa@gmail.com>
2022-02-09 13:48:53 +0200

    jwt token configuration

--- src/main/java/com/example/jwtspringsecurity/JwtSpringSecurityApplication.java
+++ src/main/java/com/example/jwtspringsecurity/JwtSpringSecurityApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,13 @@ public class JwtSpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(JwtSpringSecurityApplication.class, args);
     }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     CommandLineRunner run(UserService userService){
@@ -39,4 +48,5 @@ public class JwtSpringSecurityApplication {
             userService.addUserAuthorization("jerry", "ROLE_USER");
         };
     }
+
 }
